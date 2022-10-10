1d8a5993e525675d0f6569c6731439685c99cbe2
Jesse Phipps <jessefphipps@gmail.com>
2022-10-09 22:18:41 -0500

    Additional changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
