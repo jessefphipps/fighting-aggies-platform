eca13cd232601e89098b7206b99cae32957cfa9b
Jesse Phipps <jessefphipps@gmail.com>
2022-10-14 15:01:37 -0500

    Removed components import

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Counter from '../components/Counter';
 import FileUploader from '../components/FileUploader'
 
 document.addEventListener('DOMContentLoaded', () => {
