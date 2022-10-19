920ec567709b10d90e79989ff5fa34c88cf892bc
Ubuntu <ubuntu@ip-172-31-44-77.us-east-2.compute.internal>
2022-10-19 01:34:16 +0000

    cloud9 config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.hosts << "f0ab0606648b43769bddc0f4b5d734f3.vfs.cloud9.us-east-2.amazonaws.com"
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
