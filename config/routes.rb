44d4ea101794331c4fcf1cc03e504c4cd61082ca
Ubuntu <ubuntu@ip-172-31-23-177.us-east-2.compute.internal>
2022-10-21 04:01:02 +0000

    Fixed routing issue

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+#   # root "articles#index"
+#   root 'welcome#index'
+  
+  get 'login', to: 'login#loginPage'
+end
