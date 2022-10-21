e7dc20e1fb3fef39b4715fdcc8cd944ca9a2d35d
Ubuntu <ubuntu@ip-172-31-23-177.us-east-2.compute.internal>
2022-10-21 05:32:16 +0000

    FileUploaderNomenclature

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import FileUploader from './FileUploader'
+import FileUploader from './FileUploader/FileUploader'
 import Login from './Login'
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
