ef487655b7beb8a238de73ea2557cf1c67995acc
Ubuntu <ubuntu@ip-172-31-44-77.us-east-2.compute.internal>
2022-10-21 18:38:00 +0000

    login debug

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import FileUploader from './FileUploader/FileUploader'
-import Login from './Login'
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import LogInPage from './Login'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const App = () => {
   return (
     <Router>
       <div>
         <Route exact path={'/'} component={FileUploader}></Route>
-        <Route exact path={'/login'} component={Login}></Route>
+        <Route exact path={'/login'} component={LogInPage}></Route>
       </div>
     </Router>
   );
