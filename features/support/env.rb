cff728683b1aabcc2a25814ad690fed348048141
Jesse Phipps <jessefphipps@gmail.com>
2022-10-23 22:56:33 -0500

    Partially implemented tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -18,6 +18,7 @@
 # end
 
 require 'selenium-webdriver'
+require 'fileutils'
 
 # capabilities = Selenium::WebDriver::Remote::Capabilities.new
 # capabilities['platform'] = ENV['SELENIUM_PLATFORM'] || 'ANY'
@@ -32,12 +33,21 @@ require 'selenium-webdriver'
 # @browser = Selenium::WebDriver.for :chrome, options: options
 browser = Selenium::WebDriver.for :chrome, switches: %w[--headless --no-sandbox --disable-gpu --remote-debugin-port=9222 --screen-size=1200x800]
 
+dummy_video_path = Rails.root.join("storage", "dummy_video.mp4").to_s
+
 Before do |scenario|
 	@browser = browser
 	@login_url = "https://fightin-aggies.herokuapp.com/"
 	@dashboard_url = "https://fightin-aggies.herokuapp.com/dashboard"
+	@bad_file_type_loc = Rails.root.join("storage", "supplementary_video.jpg").to_s
+	@dummy_file_loc = dummy_video_path
+	@good_file_loc = Rails.root.join("storage", "supplementary_video.mp4").to_s
+	FileUtils.touch(dummy_video_path)
 end
 
+After do |scenario|
+	FileUtils.rm(dummy_video_path)
+end
 # at_exit do
 # 	browser.quit
 # end
