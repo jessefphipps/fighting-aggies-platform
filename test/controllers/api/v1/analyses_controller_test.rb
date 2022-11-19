499d7c09817335cf698071d76038ed5253fb71e0
Pedro Figueiredo <pedrofigueiredo@tamu.edu>
2022-11-19 03:09:45 +0000

    Adapted analyses controller tests to reflect dependency on vision

--- test/controllers/api/v1/analyses_controller_test.rb
+++ test/controllers/api/v1/analyses_controller_test.rb
@@ -36,6 +36,7 @@ class Api::V1::AnalysesControllerTest < ActionDispatch::IntegrationTest
   
   test "show with valid id" do
     last_video = Video.create(@valid_video_path) # Add temporary video
+    last_vision = Vision.create(last_video.id) # Add temporary vision
     last_analysis = Analysis.create(last_video.id)
     
     get api_v1_analyses_show_url, params: {id: last_analysis.id}
@@ -43,6 +44,7 @@ class Api::V1::AnalysesControllerTest < ActionDispatch::IntegrationTest
     assert_equal(last_analysis.to_json, @response.body, "Requested analysis does not match with query ID")
     
     last_analysis.destroy
+    last_vision.destroy # Remove temporary vision
     last_video.destroy # Remove temporary video
   end
   
@@ -59,6 +61,7 @@ class Api::V1::AnalysesControllerTest < ActionDispatch::IntegrationTest
   
   test "create two analyses for same correct video id" do
     last_video = Video.create(@valid_video_path) # Add temporary video
+    last_vision = Vision.create(last_video.id) # Add temporary vision
     
     post api_v1_analyses_create_url, params: {id: last_video.id}
     assert_response :created
@@ -67,15 +70,18 @@ class Api::V1::AnalysesControllerTest < ActionDispatch::IntegrationTest
     post api_v1_analyses_create_url, params: {id: last_video.id}
     assert_response :bad_request
     
+    last_vision.destroy
     last_video.destroy # Remove temporary video
   end
   
   test "create with correct video id after creating video" do
     last_video = Video.create(@valid_video_path) # Add temporary video
+    last_vision = Vision.create(last_video.id) # Add temporary vision
     
     post api_v1_analyses_create_url, params: {id: last_video.id}
     assert_response :created
     
+    last_vision.destroy # Remove temporary vision
     last_video.destroy # Remove temporary video
   end
   
@@ -98,6 +104,7 @@ class Api::V1::AnalysesControllerTest < ActionDispatch::IntegrationTest
   
   test "destroy with valid id" do
     last_video = Video.create(@valid_video_path) # Add temporary video
+    last_vision = Vision.create(last_video.id) # Add temporary vision
     last_analysis = Analysis.create(last_video.id)
     
     delete api_v1_analyses_destroy_url, params: {id: last_analysis.id}
@@ -105,12 +112,14 @@ class Api::V1::AnalysesControllerTest < ActionDispatch::IntegrationTest
     
     new_last_analysis = Analysis.all.order(created_at: :desc).first
     unless assert_not_equal(last_analysis.to_json, new_last_analysis.to_json, "Analysis was not deleted from DB")
+      last_vision.destroy # Manually delete in case vision is not deleted by destroy
       last_analysis.destroy # Manually delete in case video is not deleted by destroy
     end
   end
   
   test "destroying video also destroys analysis" do
     last_video = Video.create(@valid_video_path) # Add temporary video
+    last_vision = Vision.create(last_video.id) # Add temporary vision
     last_analysis = Analysis.create(last_video.id)
     
     delete api_v1_videos_destroy_url, params: {id: last_video.id}
@@ -118,6 +127,7 @@ class Api::V1::AnalysesControllerTest < ActionDispatch::IntegrationTest
     
     new_last_analysis = Analysis.all.order(created_at: :desc).first
     unless assert_not_equal(last_analysis.to_json, new_last_analysis.to_json, "Analysis was not deleted from DB when associated video was deleted")
+      last_vision.destroy # Manually delete in case vision is not deleted by destroy
       last_analysis.destroy # Manually delete in case video is not deleted by destroy
     end
   end
